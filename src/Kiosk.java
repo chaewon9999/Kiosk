5d954f677efde4854ef96d630ad9b8a394593dba
chaewon9999 <w0729@naver.com>
2025-03-11 16:07:20 +0900

    isNotNumber 메서드 생성(정수 입력 예외처리)

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -22,19 +22,12 @@ public class Kiosk {
 
             System.out.print("0. 종료\n입력: ");
 
-            // 숫자 입력 받기
-            int categoryNumber = 0;
-
-            //정수가 아닐 때 예외처리
-            while(true) {
-                try{
-                    categoryNumber = Integer.valueOf(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.print("잘못된 입력입니다. 다시 입력해주세요: ");
-                    continue;
-                }
+            // 숫자 검증
+            String inputCategory = scanner.nextLine();
+            if (isNotNumber(inputCategory)){
+                continue;
             }
+            int categoryNumber = Integer.parseInt(inputCategory);
 
             // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
             // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
@@ -55,18 +48,12 @@ public class Kiosk {
                 continue;
             }
 
-            // 숫자 입력 받기
-            int choiceMenu = 0;
-
             //입력받은 숫자가 정수가 아닐 때 예외처리
-            while(true) {
-                try{
-                    choiceMenu = Integer.valueOf(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.print("잘못된 입력입니다. 다시 입력해주세요: ");
-                }
+            String inputMenu = scanner.nextLine();
+            if (isNotNumber(inputMenu)) {
+                continue;
             }
+            int choiceMenu = Integer.parseInt(inputMenu);
 
             // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
             if (choiceMenu == 0) {
@@ -75,14 +62,12 @@ public class Kiosk {
             }
 
             try {
-
                 MenuItem menuItem = menu.getMenu().get(choiceMenu - 1);
                 System.out.println("[" + menuItem.getProductName() + "]" + "을 장바구니에 담으시겠습니까?");
                 System.out.println("1. 네 \n2. 아니오\n0. 돌아가기");
                 break;
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-                continue;
             }
 
 
@@ -92,5 +77,16 @@ public class Kiosk {
 
     void addCategory(Menu menu) {
         category.add(menu);
+    }
+
+    /**정수인지 확인**/
+    boolean isNotNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return false; //가독성을 위해 숫자가 맞다면 false를 반환해줬습니다.
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+        }
+        return true;
     }
 }
